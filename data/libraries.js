a71b6e3b5811d93786bb3a7081633df7adfef9e7
Restyled.io <commits@restyled.io>
2022-02-19 14:08:54 +0000

    Restyled by prettier

--- data/libraries.js
+++ data/libraries.js
@@ -1,89 +1,86 @@
 export const data = [
   {
-    id : 'react-three-fiber',
-    title : 'React Three Fiber',
-    github : 'https://github.com/pmndrs/react-three-fiber',
-    description : 'React-three-fiber is a React renderer for three.js',
-    image : 'https://docs.pmnd.rs/react-three-fiber/share.jpg',
-    docs : {
-      repo : 'pmndrs/react-three-fiber',
-      branch : 'master',
-      dir : 'docs',
+    id: 'react-three-fiber',
+    title: 'React Three Fiber',
+    github: 'https://github.com/pmndrs/react-three-fiber',
+    description: 'React-three-fiber is a React renderer for three.js',
+    image: 'https://docs.pmnd.rs/react-three-fiber/share.jpg',
+    docs: {
+      repo: 'pmndrs/react-three-fiber',
+      branch: 'master',
+      dir: 'docs',
     },
   },
   {
-    id : 'react-spring',
-    title : 'React Spring',
-    github : 'https://github.com/pmndrs/react-spring',
-    description :
-        'Bring your components to life with simple spring animation primitives for React',
-    image : 'https://docs.pmnd.rs/react-spring/share.jpg',
-    docs : {
-      repo : 'pmndrs/website',
-      branch : 'docs',
-      dir : 'docs/react-spring',
+    id: 'react-spring',
+    title: 'React Spring',
+    github: 'https://github.com/pmndrs/react-spring',
+    description: 'Bring your components to life with simple spring animation primitives for React',
+    image: 'https://docs.pmnd.rs/react-spring/share.jpg',
+    docs: {
+      repo: 'pmndrs/website',
+      branch: 'docs',
+      dir: 'docs/react-spring',
     },
   },
   {
-    id : 'drei',
-    title : 'Drei',
-    github : 'https://github.com/pmndrs/drei',
-    description :
-        'Drei is a growing collection of useful helpers and abstractions for react-three-fiber',
-    image : 'https://docs.pmnd.rs/logo-drei.jpg',
-    docs : {
-      repo : 'pmndrs/website',
-      branch : 'docs',
-      dir : 'docs/drei',
+    id: 'drei',
+    title: 'Drei',
+    github: 'https://github.com/pmndrs/drei',
+    description:
+      'Drei is a growing collection of useful helpers and abstractions for react-three-fiber',
+    image: 'https://docs.pmnd.rs/logo-drei.jpg',
+    docs: {
+      repo: 'pmndrs/website',
+      branch: 'docs',
+      dir: 'docs/drei',
     },
   },
   {
-    id : 'zustand',
-    title : 'Zustand',
-    github : 'https://github.com/pmndrs/zustand',
-    description :
-        'Zustand is a small, fast and scalable bearbones state-management solution, it has a comfy api based on hooks',
-    icon : '/zustand-icon.png',
-    image : 'https://docs.pmnd.rs/zustand-resources/zustand-bear.jpg',
-    docs : {
-      repo : 'pmndrs/website',
-      branch : 'docs',
-      dir : 'docs/zustand',
+    id: 'zustand',
+    title: 'Zustand',
+    github: 'https://github.com/pmndrs/zustand',
+    description:
+      'Zustand is a small, fast and scalable bearbones state-management solution, it has a comfy api based on hooks',
+    icon: '/zustand-icon.png',
+    image: 'https://docs.pmnd.rs/zustand-resources/zustand-bear.jpg',
+    docs: {
+      repo: 'pmndrs/website',
+      branch: 'docs',
+      dir: 'docs/zustand',
     },
   },
   {
-    id : 'jotai',
-    title : 'Jotai',
-    github : 'https://github.com/pmndrs/jotai',
-    description :
-        'Jotai is a primitive and flexible state management library for React',
-    icon : '/jotai-icon.png',
+    id: 'jotai',
+    title: 'Jotai',
+    github: 'https://github.com/pmndrs/jotai',
+    description: 'Jotai is a primitive and flexible state management library for React',
+    icon: '/jotai-icon.png',
     // image: 'https://docs.pmnd.rs/jotai-ghost.png',
   },
   {
-    id : 'a11y',
-    title : 'A11y',
-    github : 'https://github.com/pmndrs/react-three-a11y',
-    description :
-        '@react-three/a11y brings accessibility to webGL with easy-to-use react-three-fiber components',
-    image : 'https://docs.pmnd.rs/a11y/react-three-a11y-header.jpg',
-    docs : {
-      repo : 'pmndrs/website',
-      branch : 'docs',
-      dir : 'docs/a11y',
+    id: 'a11y',
+    title: 'A11y',
+    github: 'https://github.com/pmndrs/react-three-a11y',
+    description:
+      '@react-three/a11y brings accessibility to webGL with easy-to-use react-three-fiber components',
+    image: 'https://docs.pmnd.rs/a11y/react-three-a11y-header.jpg',
+    docs: {
+      repo: 'pmndrs/website',
+      branch: 'docs',
+      dir: 'docs/a11y',
     },
   },
   {
-    id : 'react-postprocessing',
-    title : 'React Postprocessing',
-    github : 'https://github.com/pmndrs/react-postprocessing',
-    description :
-        'React Postprocessing is a postprocessing wrapper for @react-three/fiber',
-    image : 'https://docs.pmnd.rs/react-processing.jpg',
-    docs : {
-      repo : 'pmndrs/website',
-      branch : 'docs',
-      dir : 'docs/react-postprocessing',
+    id: 'react-postprocessing',
+    title: 'React Postprocessing',
+    github: 'https://github.com/pmndrs/react-postprocessing',
+    description: 'React Postprocessing is a postprocessing wrapper for @react-three/fiber',
+    image: 'https://docs.pmnd.rs/react-processing.jpg',
+    docs: {
+      repo: 'pmndrs/website',
+      branch: 'docs',
+      dir: 'docs/react-postprocessing',
     },
   },
 ]
