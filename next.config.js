73239ea958f7eee096f1aed00a38df38ec38dc17
Restyled.io <commits@restyled.io>
2022-02-19 14:08:51 +0000

    Restyled by clang-format

--- next.config.js
+++ next.config.js
@@ -1,28 +1,28 @@
 module.exports = {
-  images: {
-    domains: ['codesandbox.io'],
+  images : {
+    domains : [ 'codesandbox.io' ],
   },
   async redirects() {
     return [
       {
-        source: '/home',
-        destination: '/',
-        permanent: true,
+        source : '/home',
+        destination : '/',
+        permanent : true,
       },
       {
-        source: '/docs/:slug*',
-        destination: '/:slug*',
-        permanent: true,
+        source : '/docs/:slug*',
+        destination : '/:slug*',
+        permanent : true,
       },
       {
-        source: '/jotai',
-        destination: 'https://jotai.pmnd.rs/docs/introduction',
-        permanent: true,
+        source : '/jotai',
+        destination : 'https://jotai.pmnd.rs/docs/introduction',
+        permanent : true,
       },
       {
-        source: '/jotai/:slug*',
-        destination: 'https://jotai.pmnd.rs/docs/:slug*',
-        permanent: true,
+        source : '/jotai/:slug*',
+        destination : 'https://jotai.pmnd.rs/docs/:slug*',
+        permanent : true,
       },
     ]
   },
